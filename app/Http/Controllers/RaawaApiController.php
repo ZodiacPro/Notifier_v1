a3424ac7f2eedabd3a279f635a6f9f0a43958ffe
Mhar Mateo <zenasmhar15@gmail.com>
2022-04-22 15:28:20 +0800

    revised active item API

--- app/Http/Controllers/RaawaApiController.php
+++ app/Http/Controllers/RaawaApiController.php
@@ -63,7 +63,7 @@ class RaawaApiController extends Controller
                 ->join('raawa_user','raawa_user.id','=','sec_log.rawwa_user_id')
                 ->join('area','area.id','raawa_user.area_id')
                 ->whereRaw('sec_log.id In (SELECT MAX(id) FROM sec_log GROUP BY rawwa_user_id)')
-                ->where('sec_log.expired','<',date('Y-m-d'))
+                ->where('sec_log.expired','>',date('Y-m-d'))
                 ->orderBy('sec_log.expired','desc')
                 ->get();
 
@@ -75,7 +75,7 @@ class RaawaApiController extends Controller
                 ->join('raawa_user','raawa_user.id','=','raawa_log.rawwa_user_id')
                 ->join('area','area.id','raawa_user.area_id')
                 ->whereRaw('raawa_log.id In (SELECT MAX(id) FROM raawa_log GROUP BY rawwa_user_id)')
-                ->where('raawa_log.expired','<',date('Y-m-d'))
+                ->where('raawa_log.expired','>',date('Y-m-d'))
                 ->orderBy('raawa_log.expired','desc')
                 ->get();
 
